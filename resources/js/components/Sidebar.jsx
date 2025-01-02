a0e800001bf8bed1502c59002fdf15991f8b2bb0
LeannaJane <leannajane2002@outlook.com>
2025-01-02 14:35:17 +0000

    Adding icons from: <a href="https://www.flaticon.com/free-icons/setting" title="setting icons">Setting icons created by Phoenix Group - Flaticon</a>

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -5,14 +5,31 @@ const Sidebar = () => {
         <div className="sidebar">
             <h2 className="logo">Eatify</h2>
             <ul className="menu">
-                <li className="menu-item">Dashboard</li>
-                <li className="menu-item">Meal Finder</li>
-                <li className="menu-item">Menu</li>
-                <li className="menu-item">Planner</li>
-                <li className="menu-item">Cabinate</li>
-                <li className="menu-item">Family Share</li>
-                <li className="menu-item">Shopping</li>
-                <li className="menu-item">Settings</li>
+                <li className="menu-item">
+                    <img src="/Images/home.png" alt="Home" className="menu-icon" />
+                    Dashboard
+                </li>
+                <li className="menu-item">
+                    <img src="/Images/search.png" alt="search" className="menu-icon" />
+                    Meal Finder</li>
+                <li className="menu-item">
+                    <img src="/Images/cutlery.png" alt="cutlery" className="menu-icon" />
+                    Menu</li>
+                <li className="menu-item">
+                    <img src="/Images/notes.png" alt="notes" className="menu-icon" />
+                    Planner</li>
+                <li className="menu-item">
+                    <img src="/Images/fridge.png" alt="fridge" className="menu-icon" />
+                    Cabinet</li>
+                <li className="menu-item">
+                    <img src="/Images/family.png" alt="family" className="menu-icon" />
+                    Family Share</li>
+                <li className="menu-item">
+                    <img src="Images/shopping.png" alt="shopping" className="menu-icon" />
+                    Shopping</li>
+                <li className="menu-item">
+                    <img src="Images/setting.png" alt="setting" className="menu-icon" />
+                    Settings</li>
             </ul>
             <div className="light-mode">
                 <span>Light Mode</span>
