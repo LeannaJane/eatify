3b61c7e017fc80ba007178637a6d2ca5caff32a9
LeannaJane <leannajane2002@outlook.com>
2025-01-12 09:17:28 +0000

    fix

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
-import Login from './Login';
+import Login from '../components/Login';
 
 const HomePage = () => {
   const { isLoggedIn } = useAuth();
